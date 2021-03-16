3ab79d14e1abfe56cfe019d494a3f8026b84292a
Warion Warter <vorontsov.r@bk.ru>
2021-03-16 23:54:28 +0700

    добавил основные страницы, добавил логику переходов, реализовал ICommand, создал стили

---
+++ Diplom/OtherClasses/Transfer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Diplom.OtherClasses
+{
+    public static class Transfer
+    {
+        public static Frame frame;
+
+        public static void GoTo(string namePage)
+        {
+            MessageBox.Show(namePage);
+        }
+    }
+}
---
+++ Diplom/VM/MainWindowVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Diplom.OtherClasses;
+using Diplom.View;
+
+namespace Diplom.VM
+{
+    public class MainWindowVM
+    {
+        public MainWindowVM()
+        {
+
+        }
+
+
+        private RelayCommand commandGoTo;
+        public RelayCommand CommandGoTo
+        {
+            get
+            {
+                return commandGoTo ?? (new RelayCommand(
+                    title =>
+                    {
+                        Transfer.GoTo(title.ToString());
+                    }));
+            }
+        }
+    }
+}
